0c6dacf798101cf689cac7975ea88a7733bc7800
Seungwoo Paek <swpaek77@gmail.com>
2022-12-28 09:58:43 +0900

    [백준] 더하기 빼기 곱하기 나누기

---
+++ backjoon/입출력과 사칙연산/1008.js
@@ -0,0 +1,5 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split(' ');
+const [a, b] = input;
+
+console.log(+a / +b);
